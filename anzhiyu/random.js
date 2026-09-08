312343e1eaa40399fa560d4c6c6f55a1cf08e422
Lop-X <fanbo01312@gmail.com>
2026-09-09 00:28:56 +0800

    Site updated: 2026-09-09 00:28:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["网站搭建/已复活/","uncategorized/考研记录（一）/","网站搭建/网站装修记录/"];function toRandomPost(){
+var posts=["网站搭建/已复活/","网站搭建/网站装修记录/","uncategorized/考研记录（一）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
