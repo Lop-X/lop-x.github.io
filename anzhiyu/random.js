ba21719b92193c0124e4d85f6bd1c09f48a37a6a
Lop-X <fanbo01312@gmail.com>
2026-09-11 00:44:16 +0800

    Site updated: 2026-09-11 00:44:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/hexo配置bangumi追番页面/","网站搭建/已复活/","网站搭建/网站装修记录/","uncategorized/考研记录（一）/"];function toRandomPost(){
+var posts=["笔记/hexo配置bangumi追番页面/","笔记/已复活/","笔记/网站装修记录/","uncategorized/考研记录（一）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
