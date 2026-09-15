4790caaf3bc3aa08d68f115ff543488ac2399287
Lop-X <fanbo01312@gmail.com>
2026-09-16 00:10:21 +0800

    Site updated: 2026-09-16 00:10:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["笔记/已复活/","笔记/常见Git命令学习/","笔记/hexo配置bangumi追番页面/","笔记/网站装修记录/","uncategorized/考研记录（一）/","笔记/用easyX制作Snake-Game/"];function toRandomPost(){
+var posts=["笔记/常见Git命令学习/","笔记/已复活/","笔记/hexo配置bangumi追番页面/","笔记/用easyX制作Snake-Game/","uncategorized/考研记录（一）/","笔记/网站装修记录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
